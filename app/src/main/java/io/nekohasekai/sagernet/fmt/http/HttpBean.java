a556a413ae1e000164230edb80933395a4b0ec85
syl139911 <143240199+syl139911@users.noreply.github.com>
2026-08-23 10:18:51 +0800

    fix: serialize path in HttpBean (parent skips path for tcp type)

--- app/src/main/java/io/nekohasekai/sagernet/fmt/http/HttpBean.java
+++ app/src/main/java/io/nekohasekai/sagernet/fmt/http/HttpBean.java
@@ -1,37 +1,32 @@
 package io.nekohasekai.sagernet.fmt.http;
 
 import androidx.annotation.NonNull;
-
 import com.esotericsoftware.kryo.io.ByteBufferInput;
 import com.esotericsoftware.kryo.io.ByteBufferOutput;
-
 import org.jetbrains.annotations.NotNull;
-
 import io.nekohasekai.sagernet.fmt.KryoConverters;
 import io.nekohasekai.sagernet.fmt.v2ray.StandardV2RayBean;
 
 public class HttpBean extends StandardV2RayBean {
-
     public String username;
-    public String ***word;
-
-    // KunBox: delHost
+    public String password;
     public Boolean delHost;
 
     @Override
     public void initializeDefaultValues() {
         super.initializeDefaultValues();
         if (username == null) username = "";
-        if (***word == null) ***word = "";
+        if (password == null) password = "";
         if (delHost == null) delHost = false;
     }
 
     @Override
     public void serialize(ByteBufferOutput output) {
-        output.writeInt(1);
+        output.writeInt(2);
         super.serialize(output);
         output.writeString(username);
-        output.writeString(***word);
+        output.writeString(password);
+        output.writeString(path);
         output.writeBoolean(delHost);
     }
 
@@ -40,30 +35,24 @@ public class HttpBean extends StandardV2RayBean {
         int version = input.readInt();
         super.deserialize(input);
         username = input.readString();
-        ***word = input.readString();
-        if (version >= 1) {
+        password = input.readString();
+        if (version >= 2) {
+            path = input.readString();
+            delHost = input.readBoolean();
+        } else if (version >= 1) {
             delHost = input.readBoolean();
         } else {
             delHost = false;
         }
     }
 
-    @NotNull
-    @Override
+    @NotNull @Override
     public HttpBean clone() {
         return KryoConverters.deserialize(new HttpBean(), KryoConverters.serialize(this));
     }
 
-    public static final Creator<HttpBean> CREATOR = new CREATOR<HttpBean>() {
-        @NonNull
-        @Override
-        public HttpBean newInstance() {
-            return new HttpBean();
-        }
-
-        @Override
-        public HttpBean[] newArray(int size) {
-            return new HttpBean[size];
-        }
+    public static final Creator<HttpBean> CREATOR = new Creator<HttpBean>() {
+        @NonNull @Override public HttpBean newInstance() { return new HttpBean(); }
+        @Override public HttpBean[] newArray(int size) { return new HttpBean[size]; }
     };
 }
